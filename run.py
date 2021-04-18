7b4f62d017a938cd3ad77a8b8956ec7a52bfd263
vysery98 <luispsi98@gmail.com>
2021-04-17 22:02:43 -0500

    Modificacion: Inicio, Suma | Se añade '/listado'

--- run.py
+++ run.py
@@ -9,12 +9,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "<h1>Hola mundo con Flask</h1>"
+    return "<h1>Hola mundo con Flask.</h1><br>\n<h2>Autor: @vysery98</h2>"
 
 @app.route('/suma')
 def suma():
-    resultado = 10 + 10
-    return "<h3>10 + 10 = %d</h3>" % (resultado)
+
+    resultado1 = 10 + 10
+    resultado2 = 20 + 20
+
+    return "<h3>10 + 10 = %d</h3>\n<h3>20 + 20 = %d</h3>" % (resultado1, resultado2)
+
+@app.route('/listado')
+def listado():
+
+    salida = ""
+    lista = ["Ingeniería Civil", "Geología", "Artes Visuales", "Tecnologías de la Información", "Arqu" + 
+    "itectura", "Computación", "Artes Escénicas", "Telecomunicaciones", "Geología"]
+
+    for i in range(len(lista)):
+        salida += ("\n\t<li>" + lista[i] + "</li>")
+
+    return "<h1><b>Carreas del Área Técnica:</b></h1>\n<h2><ul>%s\n</ul></h2>" % (salida)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
